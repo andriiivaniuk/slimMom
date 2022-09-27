9026f5a15ee832b28dd3f45618cf8ccf2f024c8d
andriiivaniuk <keed888888@gmail.com>
2022-09-27 21:05:39 +0300

    quick fix

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router";
 
-import logo from "./../../assets/Header/logo.svg"
+import logo from "./../../assets/HeaderAssets/logo.svg"
 
 import { 
     LogoWrapper,
